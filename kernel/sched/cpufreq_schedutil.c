d5db34d63e1d954a2677a9cb43d8863c4db251f9
ktommy91 <kemenesi.tamas@gmail.com>
2019-04-26 10:05:06 +0200

    schedutil: set the new up/down rates

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -804,8 +804,8 @@ static int sugov_init(struct cpufreq_policy *policy)
 	} else {
 		unsigned int lat;
 
-                tunables->up_rate_limit_us = 10000;
-                tunables->down_rate_limit_us = 20000;
+                tunables->up_rate_limit_us = 2000;
+                tunables->down_rate_limit_us = 5000;
 		tunables->iowait_boost_enable = true;
 		lat = policy->cpuinfo.transition_latency / NSEC_PER_USEC;
 		if (lat) {
